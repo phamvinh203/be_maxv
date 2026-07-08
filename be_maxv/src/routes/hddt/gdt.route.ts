4e1a1e0440cc43b4ba18022da0736570cc32e675
phamvinh203 <vinhnd203@gmail.com>
2026-07-08 15:10:23 +0700

    🎉 Add sold invoices retrieval functionality and update related types

--- be_maxv/src/routes/hddt/gdt.route.ts
+++ be_maxv/src/routes/hddt/gdt.route.ts
@@ -3,6 +3,7 @@ import {
   captcha,
   login,
   purchaseInvoices,
+  soldInvoices,
 } from "../../controllers/client/hddt/gdt.controller";
 
 export default async function (
@@ -11,4 +12,5 @@ export default async function (
   fastify.get("/captcha", captcha);
   fastify.post("/login", login);
   fastify.get("/invoices/purchase", purchaseInvoices);
+  fastify.get("/invoices/sold", soldInvoices);
 }
